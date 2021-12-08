95b05ee720faf6015647722b481d4b61897ee5c8
iago-was-here <iagoananias587@gmail.com>
2021-12-08 00:48:29 -0300

    implementando classe Lista Encadeada

---
+++ Estruturas de dados em Java/src/Listas/ListaEncadeada.java
@@ -0,0 +1,87 @@
+package Listas;
+
+public class ListaEncadeada<T> {
+	
+	No<T> referenciaEntrada;
+	
+	public ListaEncadeada(){
+		this.referenciaEntrada = null;
+	}
+	public void add(T conteudo) {
+		No<T> novoNo = new No(conteudo);
+		if(this.isEmpty()) {
+			referenciaEntrada = novoNo;
+			return;
+		}
+		
+		No<T> noAux = referenciaEntrada;
+		for(int i = 0; i < this.size()-1; i++) {
+			noAux = noAux.getProximoNo();
+		}
+		
+		noAux.setProximoNo(novoNo);
+	}
+	
+	private No<T> getNo(int index){
+		
+		validaIndice(index);
+		No<T> noAux = referenciaEntrada;
+		No<T> noRet = null;
+		
+		for(int i = 0; i < this.size()-1; i++) {
+			noRet = noAux;
+			noAux = noAux.getProximoNo();
+		}
+		
+		return noRet;
+	}
+	
+	private void validaIndice(int index) {
+		if(index >= size()) {
+			int ultIndice = size() - 1;
+			throw new IndexOutOfBoundsException("Não existe conteúdo no indiece" + index + "desta lista. Esta lista só vai ate o indice" + ultIndice + "." );
+		}
+	}
+	
+	public T get(int index){
+		return getNo(index).getConteudo();
+	}
+	
+	public T remover(int index){
+		No<T> noAlvo = this.getNo(index);
+		if(index == 0) {
+			referenciaEntrada = noAlvo.getProximoNo();
+			return noAlvo;
+		}
+		
+		No<T> noAnterior = getNo(index - 1);
+		noAnterior.setProximoNo(noAlvo.getProximoNo());
+		return noAlvo.getConteudo();
+	}
+	public int size() {
+		int tamLista = 0;
+		
+		No<T> refAux = referenciaEntrada;
+		while(true) {
+			if(refAux != null) {
+				tamLista++;
+				if(refAux.getProximoNo() != null) {
+					refAux = refAux.getProximoNo();
+				}else {
+					break;
+				}
+			}else {
+				break;
+			}
+		}
+		return tamLista;
+	}
+	
+	public boolean isEmpty() {
+		if(this.referenciaEntrada == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+}
